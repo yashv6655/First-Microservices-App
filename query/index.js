88892813a34a3830780f77d94f82c6caa66e1cfd
Yash Velagapudi <yashv6655@gmail.com>
2021-02-07 13:37:43 -0800

    Prettier formatting

--- query/index.js
+++ query/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,20 +8,20 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
-  if (type === 'PostCreated') {
+  if (type === "PostCreated") {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === 'CommentCreated') {
+  if (type === "CommentCreated") {
     const { id, content, postId } = data;
 
     const post = posts[postId];
@@ -34,5 +34,5 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4002, () => {
-  console.log('Listening on 4002');
+  console.log("Listening on 4002");
 });
